e194803803717f9c80e613fbf00d480d4a3a00cd
Sezer Huseyin <sezerceadres@gmail.com>
2022-03-25 22:21:29 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
-<?php
+<?php header('Content-Type: application/json');
+
+#include('trendyolApi_class.php');
 
 /*
 $curl = curl_init();
@@ -114,63 +116,97 @@ $item_to_trendyol = ('{
 
 */
 
-die();
+
+
 $d = array(
        'items' => array(
            0 => array(
             'barcode' =>'LD00000089196',
             'title' =>'Lora Davet Leblebi Kutusu lazer kesim ahşap kutu (1 Adet) Ceviz - 89196',
-            'productMainId'=>'89196',
-            'brandId'=>'12960',
-            'categoryId'=> 860,
+            'productMainId'=>89196,
+            'brandId'=>12960,
+            'categoryId'=> 4053,
             'quantity'=> 100,
             'stockCode'=> 'STK-345',
             'dimensionalWeight'=> 2,
-            'description'=> 'Kutu üzerine istediğiniz bilgiler yazılmaktadır. siparis NOTLARI: Lütfen İsim (Marka) ve Tarih bilgisi yazınız.',
+            'description'=> 'test',
             'currencyType'=> 'TRY',
             'listPrice'=> '239.99',
             'salePrice'=> '199.99',
             'vatRate'=> '18',
-            'cargoCompanyId'=> '10',
+            'cargoCompanyId'=> '7',
             'images'=>array(
                         array('url' => 'http://www.nesatr.com/img/urunler/89196_3.jpg'),
                     ),
             'attributes'=>array(
-                    'attributeId'=>'338',
-                    'attributeValueId'=>'6980'
+		   array('attributeId'=>'782','attributeValueId'=>'315399')
+		  
                 ),
-
         ),
-    ), 
+    )
 );
 
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.trendyol.com/sapigw/suppliers/1/v2/products",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS =>    json_encode($d),
-  CURLOPT_HTTPHEADER => array(
-   'Authorization: Basic '. base64_encode('1:1'),
-    'Content-Type: application/json',
-    
-  ),
-));
+$supplierid = "187768";
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+function request($url,$post=1,$custom=1){
+    global $d;
 
-curl_close($curl);
+    $ch = curl_init();
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_ENCODING, "");
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
+
+    if($post){
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, ($custom) ? "PUT" : "POST");
+	curl_setopt($ch,CURLOPT_POSTFIELDS,  json_encode($d));
+    }
+
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array(
+	    'Authorization: Basic '. base64_encode('VNulVs4xqzZXsfmw7OeM:GFb0z5NeFPq9yQZapqUw'),
+	    'Content-Type: application/json',
+	)
+      );
+
+
+    if($post){
+	print_r(json_encode($d,JSON_PRETTY_PRINT));
+    }else{
+	echo $url;
+    }
+
+    $response = curl_exec($ch);
+    $err = curl_error($ch);
+
+    curl_close($ch);
+
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+	    $response = json_decode($response,true);
+	    echo '<br>';
+	    print_r($response);
+	    return ($response);
+	}
+
+      
+    }
+
+#$res = request("https://api.trendyol.com/sapigw/suppliers/187768/v2/products",1,0);
+#$get_brand = request("https://api.trendyol.com/sapigw/brands",0,0);
+#$get_category = request("https://api.trendyol.com/sapigw/product-categories",0,0);
+#$get_categoryid_categoriId = request("https://api.trendyol.com/sapigw/product-categories/4053/attributes",0,0);
+#$get_categoryid_categoriId = request("https://api.trendyol.com/sapigw/product-categories/411/attributes",0,0);
+
+#$res['batchRequestId'] = '2e73dd8c-1161-4df1-88f1-015162d5f9a6-1648250292';
+if(isset($res['batchRequestId'])){
+    echo '200 ok </br>';
+    $d = request("https://api.trendyol.com/sapigw/suppliers/187768/products/batch-requests/{$res['batchRequestId']}",0,0);
+    echo '<br>';
 }
 
 ?>
