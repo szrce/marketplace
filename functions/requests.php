044388d91246d2ca1727fe5b76087f2eb6c6d362
Sezer Huseyin <sezerceadres@gmail.com>
2022-05-15 13:20:27 +0000

    Update requests.php

--- functions/requests.php
+++ functions/requests.php
@@ -19,6 +19,73 @@ function get_store_attribute($id){
             'Content-Type: application/json'
           ),
         ),
+        '4'=>
+        array('name'=>'n11','api_1'=>null,'api_2'=>null,
+          'get_order'=>
+            array(
+              'url'=>'https://api.n11.com/ws/OrderService.wsdl',
+              'method'=>'post'
+
+          ),
+          'xmloptions'=>array(
+            '{api_1}',
+            '{api_2}'
+          ),
+          'headers'=>array(
+            "content-type: text/xml"
+          ),
+        ),
+        '2'=>
+        array('name'=>'hb','api_1'=>null,'api_2'=>null,'ayar_1'=>null,'ayar_2'=>null,
+          'get_order'=>
+            array(
+              'url'=>"https://oms-external.hepsiburada.com/orders/merchantid/{merchantid}?offset=0&limit=100",
+              'method'=>'get'
+
+          ),
+          'make_package'=>
+            array(
+              'url'=>"https://oms-external.hepsiburada.com/packages/merchantid/{merchantid}",
+              'method'=>'post'
+
+          ),
+          'get_packagelist'=>
+            array(
+              'url'=>"https://oms-external.hepsiburada.com/packages/merchantid/{merchantid}?offset=1&limit=10",
+              'method'=>'get'
+
+          ),
+          'headers'=>array(
+            'authorization:Basic {ayar_1}{ayar_2}',
+            'content-type: application/json'
+          ),
+        ),
+        '10'=>
+        array('name'=>'hb_test','api_1'=>null,'api_2'=>null,'ayar_1'=>null,'ayar_2'=>null,
+          'get_order'=>
+            array(
+              'url'=>"https://oms-external-sit.hepsiburada.com/orders/merchantid/{merchantid}?offset=0&limit=100",
+              'method'=>'get'
+
+          ),
+          'make_package'=>
+            array(
+              'url'=>"https://oms-external-sit.hepsiburada.com/packages/merchantid/{merchantid}",
+              'method'=>'post'
+
+          ),
+          'get_packagelist'=>
+            array(
+              'url'=>"https://oms-external-sit.hepsiburada.com/packages/merchantid/{merchantid}?offset=1&limit=10",
+              'method'=>'get'
+
+          ),
+          'headers'=>array(
+            'authorization:Basic {ayar_1}{ayar_2}',
+            'content-type: application/json'
+          ),
+        ),
+
     );
 
 
@@ -31,6 +98,15 @@ function get_store_attribute($id){
 
     if(is_array($storeList_info[$id]['headers'])){
         $storeList_info[$id]['headers'][0] = str_replace('{api_2}',$storeList_info[$id]['api_2'],$storeList_info[$id]['headers'][0]);
+
+        if($storeList_info[$id]['name'] == 'hb' || $storeList_info[$id]['name'] == 'hb_test' ){
+            $storeList_info[$id]['headers'][0] = str_replace('{ayar_1}{ayar_2}',base64_encode("{$storeList_info[$id]['ayar_1']}:{$storeList_info[$id]['ayar_2']}"),$storeList_info[$id]['headers'][0]);
+        }
+
+    }
+    if(is_array($storeList_info[$id]['xmloptions'])){
+          $storeList_info[$id]['xmloptions'][0] = str_replace('{api_1}',$storeList_info[$id]['api_1'],$storeList_info[$id]['xmloptions'][0]);
+          $storeList_info[$id]['xmloptions'][1] = str_replace('{api_2}',$storeList_info[$id]['api_2'],$storeList_info[$id]['xmloptions'][1]);
     }
 
     return $storeList_info[$id];
@@ -42,9 +118,8 @@ function export_store_information($id){
   if(!empty($id) and !$id == NULL ){
 
     $objConn = dbConn();
-    $result = $objConn->query("SELECT api_1,api_2,pazaryeri_adi FROM pazaryeri_magaza_ayarlar WHERE  durum = 1 and id=".$id);
+    $result = $objConn->query("SELECT api_1,api_2,ayar_1,ayar_2,pazaryeri_adi FROM pazaryeri_magaza_ayarlar WHERE  durum = 1 and id=".$id);
     $arrMagaza = mysqli_fetch_array($result);
-
     return $arrMagaza;
   }
 
@@ -73,6 +148,7 @@ function Request($store_id,$request_type,$postfield=null){
       if($type == 'post'){
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
       }
+
       curl_setopt($ch, CURLOPT_ENCODING, "");
       curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
       curl_setopt($ch, CURLOPT_TIMEOUT, 30);
@@ -89,7 +165,21 @@ function Request($store_id,$request_type,$postfield=null){
         if ($err) {
           echo "cURL Error #:" . $err;
         } else {
-            return json_decode($response,true);
+          switch ($store_info['name']) {
+              case 'ciceksepeti':
+                  return json_decode($response,true);
+                  break;
+              case 'n11':
+                  return $response;
+                  break;
+              case 'hb':
+                  return json_decode($response,true);
+                  break;
+              case 'hb_test':
+                  return json_decode($response,true);
+                  break;
+            }
+            //return json_decode($response,true);
       }
 
   }
